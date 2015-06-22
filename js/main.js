cadaffc127d9c99a6599cd0fad720442ddf7840a
Michael Thiessen <electromikenetic@gmail.com>
2015-06-22 19:20:52 -0400

    Finished assignment.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ $(function() {
 
     var resize = function() {
         var NAV_BAR = 56;
-        var FILTER = 50;
         var height = $(window).height() - NAV_BAR;
-        $('.sidebar-scroll').height(height - FILTER);
+        $('.sidebar-scroll').height(height);
         $('.detail').height(height);
     };
 
@@ -22,21 +21,37 @@ var app = angular.module('app', []);
 
 app.controller('sidebar-controller', function($scope, $location, $rootScope) {
 
+    $scope.itemLimit = 25;
+
     $scope.select = function(item) {
         $rootScope.selected = item;
+
+        // Lazy loading of the edited field
+        if (item.commit.edited_message === undefined) {
+            item.commit.edited_message = item.commit.message;
+        }
+
         console.log(item);
     };
 
+    $scope.increaseItemLimit = function() {
+        $scope.itemLimit += 25;
+    }
+
     var init = function() {
         $scope.data = ALL_DATA;
-        $rootScope.selected = $scope.data[0];
-    }
+        $scope.select($scope.data[0]);
+    };
 
     init();
 
 });
 
 app.controller('detail-controller', function($scope, $location, $rootScope) {
+
+    $scope.save = function() {
+        $rootScope.selected.commit.message = $rootScope.selected.commit.edited_message;
+    };
 
 });
 
